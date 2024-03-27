cc8f2e1e43a20534f4e04a2a22edad7b867615b8
CM <CM@DESKTOP-H3OLA3G>
2024-03-27 16:34:06 -0700

    Create Jwt Classes and migrate it into database

--- SchoolSystem/Program.cs
+++ SchoolSystem/Program.cs
@@ -1,8 +1,14 @@
 
+using Application.Helper;
 using Application.IUnitOfWork;
+using Application.Models;
+using Application.Models.JwtModels;
 using Infrastructure.UnitOfWork;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using SchoolSystem.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace SchoolSystem
 {
@@ -18,11 +24,37 @@ namespace SchoolSystem
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<SchoolSystemContext>(options => options.UseSqlServer(
+            builder.Services.AddDbContext<SchoolSystemContext>(options =>
+     options.UseSqlServer(builder.Configuration.GetConnectionString("SQLConn")));
 
-                   builder.Configuration.GetConnectionString("SQLConn")
-               ));
+            //Add Jwt 
+            builder.Services.Configure<JwtClass>(builder.Configuration.GetSection("jwt"));
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLConn")));
+            builder.Services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }
 
+
+                ).AddJwtBearer(o =>
+                {
+                    o.RequireHttpsMetadata = false;
+                    o.SaveToken = false;
+                    o.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = builder.Configuration["jwt:Issuer"],
+                        ValidAudience = builder.Configuration["jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+
+                    };
+
+                });
             builder.Services.AddScoped<IUnitOfWork, SchoolUnitOfWork>();
             var app = builder.Build();
 
